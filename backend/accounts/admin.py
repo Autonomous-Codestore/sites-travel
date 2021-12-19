5f4e5c272a40fea0d1f4806be0ffdeb783ada65f
george onen <geonhib@gmail.com>
2021-12-19 13:00:51 +0300

    Modify admin.

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -5,11 +5,11 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import SignupForm, CustomUserChangeForm
 from .models import CustomUser, Index
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
+    add_form = SignupForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username',]
